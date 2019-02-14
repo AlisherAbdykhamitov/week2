7cc13dcd7d663a4ff5f60c0f5d90751ddbdd2111
AlisherAbdykhamitov <47111820+AlisherAbdykhamitov@users.noreply.github.com>
2019-02-14 20:55:28 +0600

    Update Program.cs

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,35 +1,30 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Task4
+namespace task4
 {
     class Program
     {
-        
-        public static void Place(string path)
-        {
-            DirectoryInfo directory = new DirectoryInfo(path);//dir
-            string pat = directory.Parent.FullName;// previous dir
-            using (FileStream fs = File.Create(path + "/KBTU.txt"))
-            {
-
-            }
-            File.Copy(path + "/KBTU.txt", pat + "/KBTU.txt");/*копируем существующий файл в новый*/
-
-        }
-
-
-
         static void Main(string[] args)
+
         {
-            string path = "C:/Users/Alisher/Desktop/all files/Video/Alisher"; // dir
-            Place(path);
-            File.Delete(path + "/KBTU.txt");//delete original file
+
+            string filename = "File.txt";
+            string path1 = @"C:\Users\Alisher\Desktop\all files";
+            string path2 = @"C:\Users\Alisher\Desktop\Examples";
+            string sourcefile = Path.Combine(path1, filename);
+            string destfile = Path.Combine(path2, filename); // path2 + "//" + filename
+            FileStream fs = File.Create(sourcefile); // создаем
+            fs.Close(); // закрываем , потому что мы его используем
+            File.Copy(sourcefile, destfile, true); // копируем
+            File.Delete(sourcefile); // удаляем
 
         }
+
     }
+
 }
